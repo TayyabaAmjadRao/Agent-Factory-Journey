0ae8d0ba59401c41fb3567a7fc531b860a80ec98
TayyabaAmjadRao <tayyabarao46@gmail.com>
2026-07-09 21:03:38 +0500

    Day 4 - first agent with calculator tool

--- Day4/first_agent.py
+++ Day4/first_agent.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-# Tool definition - Agent ko batao kya tools hain
+# Tool definition 
 tools = [
     {
         "type": "function",
